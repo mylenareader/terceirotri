4fc101fed561513ac28b1aa3266a2296659a58ac
mylenareader <130715968+mylenareader@users.noreply.github.com>
2024-06-07 22:46:46 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const caixaPrincipal=document.querySelector(".caixa-principal");
+const caixaPerguntas=document.querySelector(".caixa-perguntas");
+const caixaAlternativas=document.querySelector(".caixa-alternativas");
+const caixaResultado=document.querySelector(".caixa-resultado");
+const textoResultado=document.querySelector(".texto-resultado");
+
+const perguntas=[ {
+    enunciado: "Pergunta -1",
+        alternativas: [ "Alternativa -1",
+        "Alternativa 0"
+        ]
+}
+
+,
+{
+enunciado: "Pergunta 0",
+    alternativas: [ "Alternativa -1",
+    "Alternativa 0"
+    ]
+}
+
+];
